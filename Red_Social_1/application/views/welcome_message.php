b5a614e78a05441c47b0d1b5fa17e878cc71ffc3
Richardcr13 <73675614+Richardcr13@users.noreply.github.com>
2020-11-08 23:00:05 -0600

    actualización
    organización css

--- Red_Social_1/application/views/welcome_message.php
+++ Red_Social_1/application/views/welcome_message.php
@@ -1,66 +1,4 @@
 
-<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-
-
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 500px;
-		font: 18px/20px normal Helvetica, Arial, sans-serif;
-	}
-
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.descripcion {
-		text-align: center;
-		font-size: 11px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-		font: 15px/20px normal Helvetica, Arial, sans-serif;
-	}
-
-	div.login-screen{
-		border: 1px solid black;
-        margin-top: 50px;
-        margin-bottom: 100px;
-        margin-right: 450px;
-        margin-left: 280px;
-        background-color: lightblue;
-	}
-
-	div.login-form{
-        padding: 30px 40px 40px 50px;
-	}
-
-	p.pregunta{
-		padding: 15px 0px 0px 0px;
-	}
-
-	a.restablece{
-		padding: 0px 0px 0px 0px;
-	}
-
-
-	</style>
 
 <div>
 	<h1>Dragón amarillo y verde</h1>
@@ -68,7 +6,7 @@
 	<div id="body">
 
 		<p class="descripcion">Sitio creado para que puedas tener un mejor contacto con los
-		"UPIICSIANOS". Inicia sesión o registraste.</p>
+		"UPIICSIANOS". Inicia sesión o registraste</p>
 
 	</div>
 
