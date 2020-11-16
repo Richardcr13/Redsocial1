fa1d43be8d67bf73a5a389d1245f7234c4283ae2
Richardcr13 <73675614+Richardcr13@users.noreply.github.com>
2020-11-15 19:09:38 -0600

    actualizacion

--- Red_Social_1/application/views/templates/inicio/header.php
+++ Red_Social_1/application/views/templates/inicio/header.php
@@ -7,6 +7,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>Dragón Y & G</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="../CSS/inicio.css">
 	<script src="../js/jquery.js"></script>
 	<script src="../js/jquery.dataTables.min.js"></script>
